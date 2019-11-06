d49660c33ac609cace1e5513559a48a62725673b
simranchawla245 <simranchawla2496@gmail.com>
2019-11-06 01:43:55 -0800

    Committed

---
+++ Hibernate_Project/src/main/java/com/lti/Hib_ex/Hibernate_Project/Hibernate_Project/Employee.java
@@ -0,0 +1,32 @@
+package com.lti.Hib_ex.Hibernate_Project.Hibernate_Project;
+
+public class Employee {
+	private int id;
+	private String empName;
+	private String branch;
+	public Employee(int id, String empName, String branch) {
+		super();
+		this.id = id;
+		this.empName = empName;
+		this.branch = branch;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	
+}
